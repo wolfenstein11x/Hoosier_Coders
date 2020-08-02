6a9766b8f327a3815169ec06325c26e76e0ac429
charles <c.wolfe132@gmail.com>
2020-08-01 20:21:53 -0700

    Posted instructions

---
+++ challenge_1.py
@@ -0,0 +1,7 @@
+# write a function that adds two numbers together
+
+# the function must take two arguments (parameters)
+
+# the function must RETURN the sum
+
+# you should use a print statement to make sure your function works
